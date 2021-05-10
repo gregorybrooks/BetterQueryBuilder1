015a6287e1eec9c559772a3d09305c1b3f9c5148
Greg Brooks <gregorybrooks@gmail.com>
2021-05-10 10:02:23 -0400

    Version 2.0.0

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -1,3 +1,3 @@
 set -v
-docker rmi gregorybrooks/better-query-builder-1
-docker build -t gregorybrooks/better-query-builder-1 .
+docker rmi gregorybrooks/better-query-builder-1:2.0.0
+docker build -t gregorybrooks/better-query-builder-1:2.0.0 .
