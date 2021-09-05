97aabcadfbae0c0f4c8cd72dc146fa444a00dd7c
Greg Brooks <gregorybrooks@gmail.com>
2021-09-05 08:00:36 -0400

    Update

--- runit.sh
+++ runit.sh
@@ -2,4 +2,7 @@
 set -v
 cd ./programfiles
 python3 run_multipartiterank_qformulator.py --input_file=$eventExtractorFileLocation/$INPUTFILE --mode=$MODE --output_file=$queryFileLocation/$QUERYFILE --out_lang=$OUT_LANG --program_directory=$HOME/programfiles/ --phase=$PHASE
-chmod a+rw $queryFileLocation/${QUERYFILE}*
+set +e
+chmod -f a+rw $queryFileLocation/${QUERYFILE}*
+# If the chmod had a problem, ignore it
+exit 0
