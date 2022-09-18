57450d600590d0049a8181d20343e3a06a59f15c
Greg Brooks <gregorybrooks@gmail.com>
2022-09-18 09:42:53 -0400

    Use combined Chinese translation table

--- programfiles/run_multipartiterank_qformulator.py
+++ programfiles/run_multipartiterank_qformulator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d1e0daa14f7b65f6d85fcd0cf24ae7df4dbc03578b66a67072109584e022d29
-size 13695
+oid sha256:95e6e6377f1621b0f194a33f06396c590e4b93e0f2070a35eb83231af086c6da
+size 13685
