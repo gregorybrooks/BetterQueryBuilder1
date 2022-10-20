cd849119f3111f315e927a6a5ecfc45333a8ecad
Greg Brooks <gregorybrooks@gmail.com>
2022-10-20 07:30:08 -0400

    Fix a typo

--- programfiles/translation_tables/get_translation.py
+++ programfiles/translation_tables/get_translation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:130e54f21b77f69d95df9792daea6da2005d5350535200b42e080d91dcb9009d
-size 543
+oid sha256:7d334f5b9f7749adf4b3ad89f436d0d7b23497e64b30fd3914b25f845de33408
+size 541
